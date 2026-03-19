7cd48f32257c48fb582ae87214b5e811e4e99ba4
extrem765 <bojkiv1989@gmail.com>
2026-03-19 14:02:13 +0200

    data-js-slider

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,38 +1,27 @@
 import './Hero.scss'
-//import HeroCard from '@/components/HeroCard'
 
 export default () => {
   return (
     <section className="hero" aria-labelledby="hero-title">
-    
-     <div className="hero__slider swiper"> 
-       <div className="swiper-wrapper">  
-          <ul className="hero__slide-list swiper-wrapper">
-             <li className="hero__slide swiper-slide">
-              slide dsddddddddddddddddddddddddadaffffffffffffffffffffffffffffffffffffffffffffffff
-              </li>
-          
-          </ul> 
-       </div>
-
-    
-
-
-
-
-
-
-
-
-
-
-     </div>
-
-
-
-
-
+      <div className="hero__slider swiper" data-js-slider='{"loop": true, "slidesPerView": 1}'>
+        <ul className="hero__slide-list swiper-wrapper">
+          <li className="hero__slide swiper-slide">
+            <div className="hero__card">
+              122222222222
+            </div>
+          </li>
+          <li className="hero__slide swiper-slide">
+            <div className="hero__card">
+              2
+            </div>
+          </li>
+          <li className="hero__slide swiper-slide">
+            <div className="hero__card">
+              3
+            </div>
+          </li>
+        </ul>
+      </div>
     </section>
   )
-
 }
