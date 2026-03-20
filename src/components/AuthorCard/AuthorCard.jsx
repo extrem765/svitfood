b6e7a6fae4f75d6588068e635622be3cb0883bdb
extrem765 <bojkiv1989@gmail.com>
2026-03-20 19:24:14 +0200

    fix slaider

--- src/components/AuthorCard/AuthorCard.jsx
+++ src/components/AuthorCard/AuthorCard.jsx
@@ -1,6 +1,5 @@
 import './AuthorCard.scss'
 import clsx from 'clsx'
-import {Image} from 'minista'
 
 export default (props) => {
   const {
@@ -11,12 +10,11 @@ export default (props) => {
   } = props
 
   return (
-    <div
-      className={clsx('author-card', className)}
-    >
-      <Image
+    <div className={clsx('author-card', className)}>
+      <img
         className="author-card__image"
         src={imgSrc}
+        alt={name}
       />
       <div className="author-card__body">
         <h3 className="author-card__name">{name}</h3>
