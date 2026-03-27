79046fb91d9657e0c493cf0c3204fca4c06c07d5
extrem765 <bojkiv1989@gmail.com>
2026-03-27 13:21:32 +0200

    top contacn finish

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,10 +4,15 @@ export const metadata = {
   title: 'Contact',
 }
 
+const author = {
+  imgSrc: '/src/assets/images/man.jpg',
+  name: 'John Smith',
+  date: {
+    dateTime: '2022-03-15',
+    label: '15 March 2025',
+  },
+}
+
 export default () => {
-  return (
-    <>
-      <Contact />
-    </>
-  )
+  return <Contact author={author} />
 }
--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -1,3 +1,37 @@
-export default function Contact() {
-  return <div>Contact</div>
+import './Contact.scss'
+
+
+export default function Contact({ author }) {
+  return (
+    <section className="article">
+      <div className="article__container">
+
+        <h1 className="article__title">
+          Full Guide to Becoming a Professional Chef
+        </h1>
+
+        <div className="article__meta">
+          <div className="article__author">
+            <img src={author.imgSrc} alt={author.name} className="article__author-img" />
+            <span>{author.name}</span>
+          </div>
+           <span className="article__divider">|</span>
+          <time className="article__date" dateTime={author.date.dateTime}>
+            {author.date.label}
+          </time>
+        </div>
+
+        <p className="article__excerpt">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac
+          ultrices odio. Nulla at congue diam, at dignissim turpis. Ut vehicula
+          sed velit a faucibus. In feugiat vestibulum velit vel pulvinar.
+        </p>
+
+        <div className="article__image">
+          <img src="/src/assets/images/lol/1.jpg" alt="article cover" />
+        </div>
+
+      </div>
+    </section>
+  )
 }
